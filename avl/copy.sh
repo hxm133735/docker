89b11af0e3acf06f5945f46a5c1882d8c4e97292
hxm133735 <meng2nan@126.com>
2019-02-15 18:00:57 +0800

    Update copy.sh

--- avl/copy.sh
+++ avl/copy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #copy avl文件
-mkdir docker/avl/ptd20g/ docker/avl/ptd20g/ptd/ docker/avl/ptd20g/lib/ docker/avl/ptd20g/lib/engine/ docker/avl/ptd20g/lib/engine/avl3/ docker/avl/ptd20g/conf/ docker/avl/ptd20g/conf/Engine/ docker/avl/ptd20g/conf/Engine/avl3/
-cp /opt/ptd20g/ptd/filescanner docker/avl/ptd20g/ptd/
-cp -r /opt/ptd20g/lib/engine/avl3/* docker/avl/ptd20g/lib/engine/avl3
-cp /opt/ptd20g/conf/ptd.engines.ini docker/avl/ptd20g/conf/
-cp /opt/ptd20g/conf/ptd.nsq_addr.ini docker/avl/ptd20g/conf/
-cp /opt/ptd20g/conf/Engine/avl3/License.alf docker/avl/ptd20g/conf/Engine/avl3
+mkdir 
+cp 
+cp -r 
+cp 
+cp 
+cp 
